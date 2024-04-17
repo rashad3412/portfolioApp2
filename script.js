89f2824a17c33d425bd2ac4da96fcc2c28185dfa
Rashad Edwards <rashadedwards@macbook.local>
2024-04-16 20:37:23 -0400

    second commit

--- script.js
+++ script.js
@@ -18,55 +18,61 @@ const contactMe = document.querySelector("#contact-me");
 const phoneText = document.createElement("div");
 const emailText = document.createElement("div");
 const aboutBox = document.querySelector(".about");
+const aboutText = document.querySelector(".about-p");
+const appIcon = document.querySelectorAll("#app");
 
-light.addEventListener("click", () => {
-  body.style.backgroundColor = "wheat";
-  header.style.backgroundColor = "teal";
-  span.style.backgroundColor = "wheat";
-  span.style.color = "teal";
-  logo.style.backgroundColor = "wheat";
-  logo.style.color = "teal";
-  dark.style.textalign = "center";
-  h1.style.color = "white";
-  floatContainer.style.color = "black";
-  float1.style.color = "black";
-  dev.style.backgroundColor = "teal";
-  dev.style.color = "wheat";
-  home.style.color = "black";
-  about.style.color = "black";
-  ex.style.color = "#505050";
-  phone.style.backgroundColor = "teal";
-  phone.style.color = "wheat";
-  email.style.backgroundColor = "teal";
-  email.style.color = "wheat";
-  contactMe.style.color = "Black";
-  for (i = 0; i < nav.length; i++) {
-    nav[i].style.color = "white";
-  }
-});
+function lightTheme() {
+  light.addEventListener("click", () => {
+    body.style.backgroundColor = "wheat";
+    header.style.backgroundColor = "teal";
+    span.style.backgroundColor = "wheat";
+    span.style.color = "teal";
+    logo.style.backgroundColor = "wheat";
+    logo.style.color = "teal";
+    dark.style.textalign = "center";
+    h1.style.color = "white";
+    floatContainer.style.color = "black";
+    float1.style.color = "black";
+    dev.style.backgroundColor = "teal";
+    dev.style.color = "wheat";
+    home.style.color = "black";
+    about.style.color = "black";
+    ex.style.color = "#505050";
+    phone.style.backgroundColor = "teal";
+    phone.style.color = "wheat";
+    email.style.backgroundColor = "teal";
+    email.style.color = "wheat";
+    contactMe.style.color = "Black";
+    for (i = 0; i < nav.length; i++) {
+      nav[i].style.color = "white";
+    }
+  });
+}
 
-dark.addEventListener("click", () => {
-  body.style.backgroundColor = "teal";
-  header.style.backgroundColor = "wheat";
-  logo.style.backgroundColor = "teal";
-  logo.style.color = "wheat";
-  h1.style.color = "black";
-  floatContainer.style.color = "white";
-  float1.style.color = "white";
-  dev.style.backgroundColor = "wheat";
-  dev.style.color = "teal";
-  home.style.color = "white";
-  about.style.color = "white";
-  ex.style.color = "white";
-  phone.style.backgroundColor = "wheat";
-  phone.style.color = "teal";
-  email.style.backgroundColor = "wheat";
-  email.style.color = "teal";
-  contactMe.style.color = "white";
-  for (i = 0; i < nav.length; i++) {
-    nav[i].style.color = "black";
-  }
-});
+function darkTheme() {
+  dark.addEventListener("click", () => {
+    body.style.backgroundColor = "teal";
+    header.style.backgroundColor = "wheat";
+    logo.style.backgroundColor = "teal";
+    logo.style.color = "wheat";
+    h1.style.color = "black";
+    floatContainer.style.color = "white";
+    float1.style.color = "white";
+    dev.style.backgroundColor = "wheat";
+    dev.style.color = "teal";
+    home.style.color = "white";
+    about.style.color = "white";
+    ex.style.color = "white";
+    phone.style.backgroundColor = "wheat";
+    phone.style.color = "teal";
+    email.style.backgroundColor = "wheat";
+    email.style.color = "teal";
+    contactMe.style.color = "white";
+    for (i = 0; i < nav.length; i++) {
+      nav[i].style.color = "black";
+    }
+  });
+}
 
 function phoneTextDisplay() {
   phone.addEventListener("dblclick", () => {
@@ -120,8 +126,11 @@ function emailOutDisplay() {
 
 function addBoxDisplay() {
   aboutBox.addEventListener("mousemove", () => {
-    aboutBox.style.backgroundColor = "orange";
+    aboutBox.style.backgroundColor = "#F06C57";
     aboutBox.style.color = "white";
+    aboutText.style.fontSize = "20px";
+    aboutText.style.fontweight = "bold";
+    aboutText.style.letterSpacing = "3px";
   });
 }
 
@@ -129,6 +138,34 @@ function addOutBoxDisplay() {
   aboutBox.addEventListener("mouseout", () => {
     aboutBox.style.backgroundColor = "";
     aboutBox.style.color = "wheat";
+    aboutText.style.fontSize = "";
+    aboutText.style.fontFamily = "";
+    aboutText.style.letterSpacing = "";
+  });
+}
+
+function appIconDisplay() {
+  appIcon.forEach((e) => {
+    e.addEventListener("mousemove", () => {
+      e.style.backgroundColor = "#F06C57";
+      e.style.width = "45px";
+      e.style.height = "45px";
+      e.style.textAlign = "center";
+      e.style.alignItems = "center";
+      e.style.padding = "5px";
+    });
+  });
+}
+function appIconDisplayOut() {
+  appIcon.forEach((e) => {
+    e.addEventListener("mouseout", () => {
+      e.style.backgroundColor = "teal";
+      e.style.width = "";
+      e.style.height = "";
+      e.style.textAlign = "";
+      e.style.alignItems = "";
+      e.style.padding = "";
+    });
   });
 }
 
@@ -136,3 +173,9 @@ phoneTextDisplay();
 phoneOutDisplay();
 emailTextDisplay();
 emailOutDisplay();
+addBoxDisplay();
+addOutBoxDisplay();
+appIconDisplay();
+appIconDisplayOut();
+lightTheme();
+darkTheme();
